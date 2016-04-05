db356c34f0f30b56291ede465f2ea68b7689f7c0
Etienne Moutot <etienne.moutot@ens-lyon.fr>
2016-04-05 10:07:50 +0200

    Comment distance.py

--- distance.py
+++ distance.py
@@ -1,9 +1,4 @@
-""""Classifies images from an already learned database"""
-import matplotlib
-import matplotlib.pyplot as plt
-from matplotlib import cm
-
-import numpy as np
+""""Prints the distance detween two images, using our vector of features"""
 from skimage.io import imshow
 from skimage.io import imread
 
@@ -14,20 +9,23 @@ from library.feature_extraction import preprocess_image
 from sys import argv
 
 if __name__ == '__main__':
-
-    # Process image
+    # Check arguents
     if len(argv) < 3:
         print("Error: less than 2 pgm files given")
         exit()
     else:
+        # process image 1
         image1 = imread(argv[1], as_grey=True)
         p_image1 = preprocess_image(image1)
         features1 = extract_features(p_image1)
-        
+
+        # process image 2
         image2 = imread(argv[2], as_grey=True)
         p_image2 = preprocess_image(image2)
         features2 = extract_features(p_image2)
 
+
+        # make the distance
         f = features1 - features2
 
         s = 0
