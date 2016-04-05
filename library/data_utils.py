12993024b21468bc1e78d52595b7d49ed49c9a68
Etienne Moutot <etienne.moutot@ens-lyon.fr>
2016-04-05 09:57:00 +0200

    Comment data_utils

--- library/data_utils.py
+++ library/data_utils.py
@@ -7,14 +7,22 @@ from os.path import join
 from skimage.io import imread
 
 
-def load_database(data_path, labels_file):
-    """."""
-    # Read classes from file
+def load_labels(data_path, labels_file):
+    """Returns the list of labels"""
+    # Read classes from csv file
     labels = np.genfromtxt(labels_file, delimiter=',', dtype=str)
     # because of space after comma we read redundant empty column
     labels = np.array([l[0] for l in labels])
+    
+    return labels
+
+def load_database(data_path, labels_file):
+    """Loads the database"""
+    # Read classes from file
+    labels = load_labels(data_path, labels_file)
     print("%s LABELS" % str(np.unique(labels).shape[0]))
 
+    # Get all filenames
     files = list()
     for (dirpath, dirnames, filenames) in walk(data_path):
         files = filenames
@@ -25,7 +33,7 @@ def load_database(data_path, labels_file):
 
     # here we load all the images and find their label from filename
     for f in files:
-        match = re.match(label_from_name, f)
+        match = re.match(label_from_name, f) # Math tha regex of filename (*.pgm)
         if match:
             label = match.groups()[0]
             if label in labels:
@@ -39,35 +47,3 @@ def load_database(data_path, labels_file):
     print("%d DISTINCT LABELS FOUND IN DATABASE" % np.unique(data_labels).shape)
 
     return (data_images, data_labels)
-
-    
-def load_labels(data_path, labels_file):
-    """."""
-    # Read classes from file
-    labels = np.genfromtxt(labels_file, delimiter=',', dtype=str)
-    # because of space after comma we read redundant empty column
-    labels = np.array([l[0] for l in labels])
-    #print("%s LABELS" % str(np.unique(labels).shape[0]))
-
-    files = list()
-    for (dirpath, dirnames, filenames) in walk(data_path):
-        files = filenames
-
-    data_labels = list()
-    label_from_name = r"([a-zA-z0-9]+)[-_]\d+.pgm"
-
-    # here we load all the images and find their label from filename
-    for f in files:
-        match = re.match(label_from_name, f)
-        if match:
-            label = match.groups()[0]
-            if label in labels:
-                data_labels.append(label)
-        else:
-            # if name does not match our regexp or label is not in the list
-            # of classes - not read from database
-            continue
-
-    #print("%d DISTINCT LABELS FOUND IN DATABASE" % np.unique(data_labels).shape)
-
-    return data_labels
